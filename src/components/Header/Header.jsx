734a183beac0a6f8be717c450c91656486a5dbe6
Tejas Tank <tejastank10@gmail.com>
2019-06-07 00:48:17 +0530

    Feature - Added navigation bar styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  Button,
   Container,
   Collapse,
   Navbar,
@@ -30,55 +31,53 @@ export default class Header extends React.Component {
   }
   render() {
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <Container>
-            <NavbarBrand href="/">Terasology</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
-              <Nav className="ml-auto" navbar>
-                <NavItem>
-                  <NavLink href="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/blog">Blog</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/media">Media</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="/game">The Game</NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink href="https://forum.terasology.org/">Forum</NavLink>
-                </NavItem>
-                <UncontrolledDropdown nav inNavbar>
-                  <DropdownToggle nav caret>
-                    Modules
+      <Navbar light expand="md" sticky="top">
+        <Container>
+          <NavbarBrand href="/">Terasology</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/">Home</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/blog">Blog</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/media">Media</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/game">The Game</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://forum.terasology.org/">Forum</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Modules
                   </DropdownToggle>
-                  <DropdownMenu right>
-                    <DropdownItem>
-                      <NavLink href="/modules">All</NavLink>
-                    </DropdownItem>
-                    <DropdownItem>
-                      <NavLink href="/modules/gameplay-template">Gameplay Template</NavLink>
-                    </DropdownItem>
-                    <DropdownItem>
-                      <NavLink href="/modules/logic">Logic</NavLink>
-                    </DropdownItem>
-                    <DropdownItem>
-                      <NavLink href="/modules/content">Content</NavLink>
-                    </DropdownItem>
-                  </DropdownMenu>
-                </UncontrolledDropdown>
-                <NavItem>
-                  <NavLink href="/download">Download</NavLink>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </div>
+                <DropdownMenu className="slideIn animate" right>
+                  <DropdownItem>
+                    <NavLink href="/modules">All</NavLink>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <NavLink href="/modules/gameplay-template">Gameplay Template</NavLink>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <NavLink href="/modules/logic">Logic</NavLink>
+                  </DropdownItem>
+                  <DropdownItem>
+                    <NavLink href="/modules/content">Content</NavLink>
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+              <NavItem>
+                <NavLink className="btn btn-primary" href="/download">Download</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
     );
   }
 }

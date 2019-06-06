d673c20544322be0de5749760c25ee36dae4c7d0
Tejas Tank <tejastank10@gmail.com>
2019-06-07 02:16:49 +0530

    Feature - Added footer & removed unused component from header
    Removed the placeholder footer and placed a proper footer. Removed unused component "Button" from the header file.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,43 @@
 import React from "react";
 import {
     Container,
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
     Row,
     Col
-} from "reactstrap";
+} from 'reactstrap';
 
 export default class Footer extends React.Component {
     render() {
         return (
-            <footer className="page-footer mt-auto">
-                <Container>
+            <footer className="mt-auto">
+                <Container className="footer">
                     <Row>
-                        <Col>Footer</Col>
-                        <Col>Links</Col>
+                        <Col md="3"><h5>Terasology,<br />Moving Blocks!</h5></Col>
+                        <Col md="3">
+                            <ul className="footer-nav">
+                                <li><a href="/">Home</a></li>
+                                <li><a href="/blog">Blog</a></li>
+                                <li><a href="/media">Media</a></li>
+                                <li><a href="/game">The Game</a></li>
+                                <li><a href="https://forum.terasology.org/">Forum</a></li>
+                            </ul>
+                        </Col>                        
+                        <Col md="3">
+                            <ul className="footer-nav">
+                                <li><a href="/modules">Modules</a></li>
+                                <li><a href="/contact">Contact Us</a></li>
+                                <li><a href="https://www.apache.org/licenses/LICENSE-2.0">License</a></li>
+                            </ul>
+                        </Col>
+                        <Col md="3">
+                            <h5>Follow us:</h5>
+                        </Col>
                     </Row>
                 </Container>
             </footer>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Button,
   Container,
   Collapse,
   Navbar,

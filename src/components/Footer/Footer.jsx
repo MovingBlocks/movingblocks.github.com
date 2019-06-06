e0e3acdc71feacb0fb690daedfb225f09dc6a28c
Tejas Tank <tejastank10@gmail.com>
2019-06-07 04:12:36 +0530

    Feature - Added Icons to the footer
    Added social media icons in the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,16 +1,21 @@
 import React from "react";
+import { IconContext } from "react-icons";
 import {
     Container,
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
     Row,
     Col
 } from 'reactstrap';
+import {
+    FaGithubSquare,
+    FaFacebookSquare,
+    FaTwitterSquare,
+    FaDiscord,
+    FaYoutubeSquare,
+    FaRedditSquare,
+    FaGooglePlusSquare,
+    FaComments
+} from "react-icons/fa";
+
 
 export default class Footer extends React.Component {
     render() {
@@ -21,22 +26,62 @@ export default class Footer extends React.Component {
                         <Col md="3"><h5>Terasology,<br />Moving Blocks!</h5></Col>
                         <Col md="3">
                             <ul className="footer-nav">
-                                <li><a href="/">Home</a></li>
-                                <li><a href="/blog">Blog</a></li>
-                                <li><a href="/media">Media</a></li>
-                                <li><a href="/game">The Game</a></li>
-                                <li><a href="https://forum.terasology.org/">Forum</a></li>
-                            </ul>
-                        </Col>                        
-                        <Col md="3">
-                            <ul className="footer-nav">
-                                <li><a href="/modules">Modules</a></li>
-                                <li><a href="/contact">Contact Us</a></li>
-                                <li><a href="https://www.apache.org/licenses/LICENSE-2.0">License</a></li>
+                                <li><a className="link" href="/">Home</a></li>
+                                <li><a className="link" href="/blog">Blog</a></li>
+                                <li><a className="link" href="/media">Media</a></li>
+                                <li><a className="link" href="/game">The Game</a></li>
+                                <li><a className="link" href="https://forum.terasology.org/">Forum</a></li>
                             </ul>
                         </Col>
                         <Col md="3">
-                            <h5>Follow us:</h5>
+                            <ul className="footer-nav">
+                                <li><a className="link" href="/modules">Modules</a></li>
+                                <li><a className="link" href="/contact">Contact Us</a></li>
+                                <li><a className="link" href="https://www.apache.org/licenses/LICENSE-2.0">License</a></li>
+                            </ul>
+                        </Col>
+                        <Col md="3">
+                            <h5>Get in touch:</h5>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="https://github.com/MovingBlocks/Terasology">
+                                    <FaGithubSquare />
+                                </a>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="https://www.facebook.com/Terasology">
+                                    <FaFacebookSquare />
+                                </a>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="https://twitter.com/terasology">
+                                    <FaTwitterSquare />
+                                </a>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="https://discord.gg/terasology">
+                                    <FaDiscord />
+                                </a>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="https://www.youtube.com/user/blockmaniaTV">
+                                    <FaYoutubeSquare />
+                                </a>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="https://reddit.com/r/Terasology">
+                                    <FaRedditSquare />
+                                </a>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="https://plus.google.com/103835217961917018533">
+                                    <FaGooglePlusSquare />
+                                </a>
+                            </IconContext.Provider>
+                            <IconContext.Provider value={{ size: "2.5em" }}>
+                                <a className="link" href="http://webchat.freenode.net/?channels=terasology&uio=d4?channels=%23terasology&nick=Terasologist...&prompt=1&useUserListIcons=true">
+                                    <FaComments />
+                                </a>
+                            </IconContext.Provider>
                         </Col>
                     </Row>
                 </Container>

1d556d9fd2688f0506f3930a91e4e19ad938e47c
adelinetush <adelinetush@gmail.com>
2019-10-03 11:32:51 +0300

    Added Media Page

---
+++ src/components/Media/Media.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Media.css";
+import Img from "gatsby-image"
+import {useStaticQuery, graphql} from "gatsby"
+
+export default () => (
+  <div className="media">
+    <h1>Screenshots</h1>
+  </div>
+);
+
+
+
---
+++ src/pages/media.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Helmet from "react-helmet";
+import Layout from "../layout";
+import Media from "../components/Media/Media";
+import config from "../../data/SiteConfig";
+
+
+export default () => (
+  <Layout>
+    <div className="media-container">
+      <Helmet title={`Media | ${config.siteTitle}`} />
+      <Media />
+    </div>
+  </Layout>
+);

6eae6d39d293af3314f1780e76329e1f77906a7e
adelinetush <adelinetush@gmail.com>
2019-10-03 13:12:46 +0300

    Removed extra lines, unused iports, package-lock file

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import "./Media.css";
-import Img from "gatsby-image"
-import {useStaticQuery, graphql} from "gatsby"
 
 export default () => (
   <div className="media">
     <h1>Screenshots</h1>
   </div>
 );
-
-
-

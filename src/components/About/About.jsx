43ee9c978850d06f9e712ebac9c1082a479ba071
grace <obieri.grace@gmail.com>
2019-10-22 10:07:47 +0100

    Implement requested changes using reactstrap and chaning content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Row, Col } from 'reactstrap';
 import "./About.css";
-import modding from './images/mods.jpg';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 import api from './images/mod2.png';
 import community from './images/community.jpg';
 
@@ -8,73 +10,85 @@ export default () => (
   <div className="about">
     <div className="about_bg">
       <div className="about__bg-overlay">
-        <div className="row justify-content-center">
-          <div className="col-md-10 mt-5">
+        <Row className="justify-content-center">
+          <Col md="10" className="mt-5">
             <h1>Terasology</h1>
               <p className="mt-5"> An open source voxel world  born from a Minecraft-inspired tech demo 
                 <br/>
                 Imagine the possibilities! 
               </p>
-              <a href="https://github.com/Terasology/terasology.github.io" className="btn btn-light btn-lg mt-5 px-5 font-weight-bold ">Find out More</a>
-          </div>
-        </div>
+              <a href="https://github.com/Terasology/terasology.github.io" className="btn btn-light btn-lg mt-5 px-5 font-weight-bold ">Find out  More</a>
+          </Col>
+        </Row>
       </div>
     </div>
     <section className="about__details py-5 mt-6e">
-      <div className="row justify-content-center">
-        <div className="col-md-8">
+      <Row className="justify-content-center">
+        <Col md="8">
           <h2 className="mb-5 text-center">About Us</h2>
           <p className="text-center">
             The Terasology project was and is becoming a stable platform for various types of gameplay settings in a voxel world. 
             The creators and maintainers are a diverse mix of software developers, designers, game testers, 
             graphic artists, and musicians. We encourage others to join
           </p>
-        </div>
-      </div>
-      <div className="row justify-content-center">
-        <div className="col-md-8">
-          <div className="row mt-6e">
-            <div className="col-md-5 pt-5">
-            <div className="mod_bg"></div>
-            </div>
-            <div className="col-md-7">
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <Row className="mt-6e">
+            <Col md="5" className="pt-5">
+              {/* <Img /> */}
+              <div className="mod_bg"></div>
+            </Col>
+            <Col md="7">
               <h3 className="mb-3 text-right mr-4">History</h3>
               <p className="text-justify">
+              Founded in 2011 by Benjamin "Begla" Glatzel while researching procedural terrain generation and effective 
+              rendering techniques, He succeded in creating a minecraft like demo
               From the ground up, Terasology was built to be a super hackable and modular game. 
               We host a large number of modules under the Terasology organization and many more which 
               are maintained by individual enthusiasts. We welcome new ideas, both crazy and well thought-out 
               for modules and game extensions from anyone and everyone, so feel free to talk to us on our <a className="text-success font-weight-bold" href="https://discordapp.com/invite/Terasology">Discord</a> or IRC (#terasology on Freenode).
               </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="row justify-content-center">
-        <div className="col-md-8">
-          <div className="row mt-6e">
-            <div className="col-md-7">
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <Row className="mt-6e">
+            <Col md="7">
               <h3 className="mb-3  mr-4">Modding API</h3>
               <p className="text-justify">
-                Terasology is fully open source and is licensed under the Apache 2.0 
-                license for code and under CC BY 4.0 for artwork (see credits for some minor exceptions).
-                The Terasology project was and is becoming a stable platform for various types of gameplay settings in a voxel world. 
-                The creators and maintainers are a diverse mix of software developers, designers, game testers, 
-                graphic artists, and musicians. We encourage others to join
+                Terasology's engine uses a whitelisting approach to expose an API for modules using two primary methods 
+                and a rarely needed third one:
               </p>
-            </div>
+              <ul>
+                <li>Classes or packages marked with the @API annotation</li>
+                <li>Classes or packages in the basic whitelist defined in ExternalApiWhitelist.java</li>
+                <li>
+                  Rarely blocks of code in the engine may be hit in a way requiring use of AccessController.doPrivileged(...) 
+                  usually module authors do not need to worry about this but once in a while it could explain something quirky
+                </li>
+              </ul>
+              <div className="text-center">
+                <a className="font-weight-bold btn btn-lg btn-success mt-5" href="https://github.com/MovingBlocks/Terasology/wiki/Modding-API">Learn More</a>
+              </div>
+                
+            </Col>
             <div className="col-md-5 pt-5">
               <div className="mod_api-bg"></div>
             </div>
-          </div>
-        </div>
-      </div>
-      <div className="row justify-content-center">
-        <div className="col-md-8">
-          <div className="row mt-6e">
-            <div className="col-md-5 pt-5">
+          </Row>
+        </Col>
+      </Row>
+      <Row className="justify-content-center">
+        <Col md="8">
+          <Row className="mt-6e">
+            <Col md="5" className="pt-5">
               <div className="mod_group"></div>
-            </div>
-            <div className="col-md-7">
+            </Col>
+            <Col md="7">
               <h3 className="mb-3 text-right mr-4">Terasology Community</h3>
               <p className="text-justify">
               The creators and maintainers are a diverse mix of software developers, designers, 
@@ -83,12 +97,12 @@ export default () => (
               and welcoming as possible to newcomers.
               If you have any questions or if you just want to chat use this invite link for our <a className="text-success font-weight-bold" href="https://discordapp.com/invite/Terasology">Discord</a>, or check out our 
               IRC channel #terasology on Freenode. And don't worry about missing something by 
-              joining only one of those, as we have everything bridged. :)
+              joining only one of those, as we have everything bridged.
               </p>
-            </div>
-          </div>
-        </div>
-      </div>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </section>
   </div>
 );

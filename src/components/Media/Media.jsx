a3b1c1c3f75e4821a841b4cdaffb30193061121f
adelinetush <adelinetush@gmail.com>
2019-11-06 14:50:05 +0300

    Issue #17 Implement Additional Pages/Add More Content

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Media.css";
 import Img from "gatsby-image"
-import {useStaticQuery, graphql} from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Gallery = () => {
-  const data = useStaticQuery( graphql`
+  const data = useStaticQuery(graphql`
   query Images{
     images: allFile(filter: {relativeDirectory: { eq: "images"} }){
       nodes {
         id
+        name
         childImageSharp {
           fixed(width: 200, height: 200) {
             ...GatsbyImageSharpFixed
+          }
+          fluid(maxWidth: 800) {
+            src
           }
       }
     }
@@ -19,13 +23,38 @@ const Gallery = () => {
   }
 `
   )
+  console.log(data);
   return (
-    <div>
-    {data.images.nodes.map(image => (
-      <Img key={image.id} fixed={image.childImageSharp.fixed}></Img>
-    ))}
-    </div>
+    <section>
+      <div>
+        {data.images.nodes.map(image => (
+          <button class="btnImage" onClick={() => ZoomImages(image.childImageSharp.fluid.src)}>
+            <Img class="images" key={image.id} fixed={image.childImageSharp.fixed}></Img>
+          </button>
+        ))}
+      </div>
+      <div id="imageModal" class="modal">
+        <img class="modal-content" id="imageZoom"></img>
+      </div>
+    </section>
   )
 }
 
 export default Gallery
+
+var imageZoomed = false;
+
+function ZoomImages(src) {
+  var modal = document.getElementById("imageModal");
+  var modalImg = document.getElementById("imageZoom");
+  imageZoomed = true;
+  modal.style.display = "block";
+  modalImg.src = src;
+}
+
+document.addEventListener("click", function(){
+  if(imageZoomed == true){
+    document.getElementById('imageModal').style.display = 'none';
+    imageZoomed = false;
+  }
+});

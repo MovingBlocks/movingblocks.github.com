899a090f78dcb3ff090506d71dff9e4b93491318
adelinetush <adelinetush@gmail.com>
2019-11-08 23:18:28 +0300

    adding css to media page

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Media.css";
 import Img from "gatsby-image"
+import { Row, Col } from 'reactstrap';
 import { useStaticQuery, graphql } from "gatsby"
 
 const Gallery = () => {
@@ -11,7 +12,7 @@ const Gallery = () => {
         id
         name
         childImageSharp {
-          fixed(width: 200, height: 200) {
+          fixed(width: 240, height: 240) {
             ...GatsbyImageSharpFixed
           }
           fluid(maxWidth: 800) {
@@ -26,16 +27,20 @@ const Gallery = () => {
   console.log(data);
   return (
     <section>
-      <div>
-        {data.images.nodes.map(image => (
-          <button class="btnImage" onClick={() => ZoomImages(image.childImageSharp.fluid.src)}>
-            <Img class="images" key={image.id} fixed={image.childImageSharp.fixed}></Img>
-          </button>
-        ))}
-      </div>
-      <div id="imageModal" class="modal">
-        <img class="modal-content" id="imageZoom"></img>
-      </div>
+      <Row className="justify-content-center">
+        <Col lg="8">
+          <div>
+            {data.images.nodes.map(image => (
+              <button class="btnImage" onClick={() => ZoomImages(image.childImageSharp.fluid.src)}>
+                <Img class="images" key={image.id} fixed={image.childImageSharp.fixed}></Img>
+              </button>
+            ))}
+          </div>
+          <div id="imageModal" class="modal">
+            <img class="modal-content" id="imageZoom"></img>
+          </div>
+        </Col>
+      </Row>
     </section>
   )
 }
@@ -52,8 +57,8 @@ function ZoomImages(src) {
   modalImg.src = src;
 }
 
-document.addEventListener("click", function(){
-  if(imageZoomed == true){
+document.addEventListener("click", function () {
+  if (imageZoomed == true) {
     document.getElementById('imageModal').style.display = 'none';
     imageZoomed = false;
   }

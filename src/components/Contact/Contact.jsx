6f64abc060f12a19a1fa81e7498d74e24ae1f711
Prathamesh Sahasrabhojane <ssahasrabhojane@gmail.com>
2020-03-20 19:40:59 +0530

    Optimized Code

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Row, Col } from 'reactstrap';
 import "./Contact.css";
 import { IconContext } from "react-icons";
-import { graphql, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
+
 import {
   FaGithubSquare,
   FaFacebookSquare,
@@ -16,37 +15,12 @@ import {
 } from "react-icons/fa";
 
 
-export default props => {
-  const data = useStaticQuery(graphql`
-    query myImages2{
-      historyImg: file(relativePath: {eq: "images/3.jpg"}){
-        childImageSharp{
-          fluid(maxWidth: 300){
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-      }
-    },
-    modding: file(relativePath: {eq: "images/9.jpg"}){
-      childImageSharp{
-        fluid(maxWidth: 300){
-          ...GatsbyImageSharpFluid
-        }
-      }
-    },
-    community: file(relativePath: {eq: "images/8.jpg"}){
-      childImageSharp{
-        fluid(maxWidth: 300){
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
-`);
-
+export default () => {
+  
   return (
     <div className="contact">
       <div className="contact_bg">
-        <div className="contact__bg-overlay">
+        <div className="contact_bg-overlay">
           <Row className="justify-content-center">
             <Col md="10" className="justify-content-center">
               <h1>Contact Us</h1>
@@ -54,92 +28,123 @@ export default props => {
           </Row>
         </div>
       </div>
-      <section className="contact__details py-5 mt-6e">
+      <section className="contact_details py-5 mt-6e">
         
-      <Row>
-           <IconContext.Provider value={{ size: "6em"}}>
+        <center> 
+          <h5>Be part of the community by joining the following handles</h5>
+          <br />
+        </center>
+        <Row>
+          <IconContext.Provider value={{ size: "4em"}}>
             <a 
-            style={{padding:"7px"}}
+              style={{padding:"5px"}}
               className="link"
               href="https://github.com/MovingBlocks/Terasology"
             >
-              <FaGithubSquare />
-              <br></br>
-             <center> Github</center> 
+              <center>
+                <FaGithubSquare style={{color:'#000000'}} />
+                <br />
+                Github
+              </center> 
             </a>
             
           </IconContext.Provider>
-          <IconContext.Provider value={{ size: "6em" }}>
+          <IconContext.Provider value={{ size: "4em" }}>
             <a 
-            style={{padding:"7px"}} 
-            className="link" href="https://www.facebook.com/Terasology">
-              <FaFacebookSquare />
-              <br></br>
-             <center> Facebook</center> 
+              style={{padding:"5px"}} 
+              className="link"
+              href="https://www.facebook.com/Terasology"
+            >
+              <center> 
+                <FaFacebookSquare style={{color:'#3b5998'}} />
+                <br />
+                Facebook
+              </center> 
             </a>
           </IconContext.Provider>
-          <IconContext.Provider value={{ size: "6em" }}>
+          <IconContext.Provider value={{ size: "4em" }}>
             <a 
-            style={{padding:"7px"}} 
-            className="link" href="https://twitter.com/terasology">
-              <FaTwitterSquare />
-              <br></br>
-             <center> Twitter</center> 
+              style={{padding:"5px"}} 
+              className="link"
+              href="https://twitter.com/terasology"
+            >
+              <center> 
+                <FaTwitterSquare style={{color:'#00acee'}} />
+                <br />
+                Twitter
+              </center> 
             </a>
           </IconContext.Provider>
-          <IconContext.Provider value={{ size: "6em" }}>
+          <IconContext.Provider value={{ size: "4em" }}>
             <a 
-            style={{padding:"7px"}}
-            className="link" href="https://discord.gg/terasology">
-              <FaDiscord />
-              <br></br>
-             <center> Discord </center> 
+              style={{padding:"5px"}}
+              className="link"
+              href="https://discord.gg/terasology"
+            >
+              <center> 
+                <FaDiscord style={{color:'#7289da'}} />
+                <br />
+                Discord
+                {' '}
+              </center> 
             </a>
           </IconContext.Provider>
-          <IconContext.Provider value={{ size: "6em" }}>
+          <IconContext.Provider value={{ size: "4em" }}>
             <a
-            style={{padding:"7px"}}
+              style={{padding:"5px"}}
               className="link"
               href="https://www.youtube.com/user/blockmaniaTV"
             >
-              <FaYoutubeSquare />
-              <br></br>
-             <center> Youtube </center> 
+              <center> 
+                <FaYoutubeSquare style={{color:'#c4302b'}} />
+                <br />
+                Youtube
+                {' '}
+              </center> 
             </a>
           </IconContext.Provider>
-          <IconContext.Provider value={{ size: "6em" }}>
+          <IconContext.Provider value={{ size: "4em" }}>
             <a 
-            style={{padding:"7px"}}
-            className="link" href="https://reddit.com/r/Terasology">
-              <FaRedditSquare />
-              <br></br>
-             <center> Reddit  </center> 
+              style={{padding:"5px"}}
+              className="link"
+              href="https://reddit.com/r/Terasology"
+            >
+              <center> 
+                <FaRedditSquare style={{color:'#FF4301'}} />
+                <br />
+                Reddit
+                {' '}
+              </center> 
             </a>
           </IconContext.Provider>
-          <IconContext.Provider value={{ size: "6em" }}>
+          <IconContext.Provider value={{ size: "4em" }}>
             <a
-            style={{padding:"7px"}}
+              style={{padding:"5px"}}
               className="link"
               href="https://plus.google.com/103835217961917018533"
             >
-              <FaGooglePlusSquare />
-              <br></br>
-             <center> Google Plus</center> 
+              <center> 
+                <FaGooglePlusSquare style={{color:'#db4a39'}} />
+                <br />
+                Google Plus
+              </center> 
             </a>
           </IconContext.Provider>
-          <IconContext.Provider value={{ size: "6em" }}>
+          <IconContext.Provider value={{ size: "4em" }}>
             <a 
-            style={{padding:"7px"}}
+              style={{padding:"5px"}}
               className="link"
               href="http://webchat.freenode.net/?channels=terasology&uio=d4?channels=%23terasology&nick=Terasologist...&prompt=1&useUserListIcons=true"
             >
-              <FaComments />
-              <br></br>
-             <center> Webchat</center> 
+              <center>  
+                <FaComments style={{color:'#7bb32e'}} />
+                <br />
+                Webchat
+              </center> 
             </a>
           </IconContext.Provider>
        
-      </Row>        
+        </Row>        
       </section>
     </div>
   )

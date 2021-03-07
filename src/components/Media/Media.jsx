99f8905e2fe662e59e6354dd802d08ed26e59f49
DIVYANSHU RAJ <43696525+endurance21@users.noreply.github.com>
2021-03-07 21:43:52 +0530

    fix(gallery): implemented `onClick` event using react hooks

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React ,{useEffect, useRef} from "react";
 import "./Media.css";
 import Img from "gatsby-image"
 import { Row, Col } from 'reactstrap';
 import { useStaticQuery, graphql } from "gatsby"
 
 const Gallery = () => {
+  const imageZoomed = useRef()
+  const modal = useRef()
+  const modalImg = useRef()
+
+  useEffect(() => {
+    document.addEventListener("click", (event)=>{
+      if(event.target == modal.current && imageZoomed.current == true){
+        modal.current.style.display = 'none'
+        imageZoomed.current = false
+      }
+      
+    });
+  }, []);
+
+
+  const ZoomImages = (src)=>{
+    imageZoomed.current = true
+    modal.current.style.display = 'block'
+    modalImg.current.src = src
+  }
   const data = useStaticQuery(graphql`
   query Images{
     images: allFile(filter: {relativeDirectory: { eq: "images"} }){
@@ -31,13 +51,13 @@ const Gallery = () => {
         <Col lg="8">
           <div>
             {data.images.nodes.map(image => (
-              <button class="btnImage" onClick={() => ZoomImages(image.childImageSharp.fluid.src)}>
+              <button class="btnImage" onClick={(e) => ZoomImages(image.childImageSharp.fluid.src,e)}>
                 <Img class="images" key={image.id} fixed={image.childImageSharp.fixed}></Img>
               </button>
             ))}
           </div>
-          <div id="imageModal" class="modal">
-            <img class="modal-content" id="imageZoom"></img>
+          <div id="imageModal" class="modal" ref={modal}>
+            <img class="modal-content" id="imageZoom" ref={modalImg}></img>
           </div>
         </Col>
       </Row>
@@ -47,19 +67,3 @@ const Gallery = () => {
 
 export default Gallery
 
-var imageZoomed = false;
-
-function ZoomImages(src) {
-  var modal = document.getElementById("imageModal");
-  var modalImg = document.getElementById("imageZoom");
-  imageZoomed = true;
-  modal.style.display = "block";
-  modalImg.src = src;
-}
-
-document.addEventListener("click", function () {
-  if (imageZoomed == true) {
-    document.getElementById('imageModal').style.display = 'none';
-    imageZoomed = false;
-  }
-});

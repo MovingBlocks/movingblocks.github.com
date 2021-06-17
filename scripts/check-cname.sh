c8647c240112d83df9e40e6a894b9c2eeb80ca4b
ryuk156 <yp15601560@gmail.com>
2021-06-17 12:03:21 +0530

    fix: failing Cname script

--- scripts/check-cname.sh
+++ scripts/check-cname.sh
@@ -4,8 +4,14 @@ echo "Checking for CNAME"
 
 File="CNAME"
 DST="./public"
+fileFound=0
 for i in $(ls ./*); do
-    if [ -f "$File"]; then
+    if [ -f "$File" ]; then
         cp $File "$DST"
+        fileFound=fileFound+1
     fi
 done
+
+if [ fileFound==0 ]; then
+    echo "$File not Found"
+fi

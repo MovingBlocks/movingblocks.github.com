db149cf914a40d0f23ddc257b3d08d9249d66515
ryuk156 <yp15601560@gmail.com>
2021-06-19 15:43:20 +0530

    fix: refactored frontmatter.py & improved module loading in scrape.py

--- Module-generation/frontmatter.py
+++ Module-generation/frontmatter.py
@@ -1,11 +1,8 @@
 import os
 import json
 
-
-
 DIR="./meta-data/"
 DST="./modules/"
-
 os.mkdir(DST)
 
 for folders in  os.listdir(DIR):
@@ -14,80 +11,59 @@ for folders in  os.listdir(DIR):
         getModuledata = open(moduleFile,"r")
         readModuleData=getModuledata.read()
         parseData=json.loads(readModuleData)
-        
-    
         moduleName=parseData['id']
         moduleDescription=parseData['description']
         
-       
-        
-
-        if "isGameplay" in parseData:
-            gameplay=parseData['isGameplay']
-        else:
-            gameplay=False
-
-        if "isServerSideOnly" in parseData:
-            serverside=parseData['isServerSideOnly']
-        else:
-            serverside=False
-
-        if "isWorld" in parseData:
-            world=parseData['isWorld']
-        else:
-            world=False
-
-        if "isAugmentation" in parseData:
-            augment=parseData['isAugmentation']
-        else:
-            augment=False
-        
-        if "isLibrary" in parseData:
-            library=parseData['isLibrary']
-        else:
-            library=False
-        if "isAsset" in parseData:
-            asset=parseData['isAsset']
-        else:
-            asset=False
-
-        if "isSpecific" in parseData:
-            specific=parseData['isSpecific']
-        else:
-            specific=False
-
-
-        moduleLogo="./logo.jpg"
+        moduleLogo="logo.jpg"
         moduleReadme=DIR+folders+"/README.md"
 
         os.mkdir(DST+moduleName)
         IndexMd=open(DST+moduleName+"/index.md","a+")
         IndexMd.write('---\n')
-        IndexMd.write('posttype: module \n')
+        IndexMd.write('posttype: "module" \n')
         IndexMd.write('title: '+moduleName+'\n')
-        IndexMd.write('description: '+moduleDescription+'\n')
+        IndexMd.write('description: "'+moduleDescription+'"\n')
 
-        if(os.path.isfile(moduleFile)):
-            IndexMd.write('cover: '+moduleLogo+'\n')
-        
+        if(os.path.isfile(moduleLogo)):
+            print("yes")
+            IndexMd.write('cover: "./logo.jpg"'+'\n')
+            sourceImage=open(moduleLogo, "rb")
+            readImage=sourceImage.read()
+            ImageFile = open(DST+moduleName+"/logo.jpg", "wb+")    
+            ImageFile.write(readImage)
+            ImageFile.close()
+            sourceImage.close()
+
         Tags=[]
-        if(serverside):
-            Tags.append("Server")
-        if (gameplay):
-            Tags.append("Gameplay")
-        if (world):
-            Tags.append("World")
-        if (augment):
-            Tags.append("Augment")
-        if (library):
-            Tags.append("Library")
-        if (asset):
-            Tags.append("Asset")
-        if (specific):
-            Tags.append("Specific")
+        if "isGameplay" in parseData:
+            if (parseData['isGameplay']):
+                Tags.append("Gameplay")
+
+        if "isServerSideOnly" in parseData:
+            if(parseData['isServerSideOnly']):
+                Tags.append("Server")
+
+        if "isWorld" in parseData:
+            if (parseData['isWorld']):
+                Tags.append("World")
+
+        if "isAugmentation" in parseData:
+            if (parseData['isAugmentation']):
+                Tags.append("Augment")
         
+        if "isLibrary" in parseData:
+            if(parseData['isLibrary']):
+                Tags.append("Library")
+       
+        if "isAsset" in parseData:
+            if(parseData['isAsset']):
+                Tags.append("Asset")
+
+        if "isSpecific" in parseData:
+            if(parseData['isSpecific']):
+                Tags.append("Specific")
+
         IndexMd.write("tags: "+"["+','.join(f'"{w}"' for w in Tags)+"]\n")
-        
         IndexMd.write('---\n')
 
         if(os.path.isfile(moduleReadme)):
--- Module-generation/scrape.py
+++ Module-generation/scrape.py
@@ -3,85 +3,82 @@ import json
 import requests
 from github import Github
 
-IndexDir="./meta-data/"
+IndexDir = "./meta-data/"
 os.mkdir(IndexDir)
 
-accessToken=os.environ.get('GIT_CREDS')
+accessToken = os.environ.get('GIT_CREDS')
 g = Github(accessToken)
 
-
-user= g.get_user('Terasology')
+user = g.get_user('Terasology')
 repo = user.get_repo("Index")
 
 contents = repo.get_contents("/distros/omega/gradle.properties")
-x=contents.decoded_content.decode()
+string = contents.decoded_content.decode()
 
-# Filter Modules and Convert it to array
-f = open('Modules.txt', 'w')
-f.write(x)
-f.close()
-f = open("Modules.txt", "r")
-lines=f.readlines()
-line=lines[1]
-word1='extraModules='
-word2='\n'
-wordreplace=line.replace(word1, "")
-wordreplace=wordreplace.replace(word2,"")
-Modules=wordreplace.split(",")
-
+filterData = string.split("\n", 1)[1]
+filterData = filterData.rsplit("\n", 5)[0]
+word1 = 'extraModules='
+word2 = '\n'
+wordreplace = filterData.replace(word1, "")
+wordreplace = wordreplace.replace(word2, "")
+Modules = wordreplace.split(",")
 
 # Collect Modules
-for i in range(0,len(Modules)):
-   if(i<len(Modules)):
-      repository= user.get_repo(Modules[i])
-      con=repository.get_contents("")
-      for content_file in con:
-          if(content_file.path=="module.txt"):
-              getModuleCon=repository.get_contents("module.txt")
-              getData=getModuleCon.decoded_content.decode()
-              parseData=json.loads(getData)
-              ModuleName=parseData['id']
-              ModuleDir=os.mkdir(IndexDir+ModuleName)
-              ModuleDirSrc=IndexDir+ModuleName
+for i in range(0, len(Modules)):
+    if(i < (len(Modules))):
+        repository = user.get_repo(Modules[i])
+        con = repository.get_contents("")
+        for content_file in con:
+            if(content_file.path == "module.txt"):
+                getModuleCon = repository.get_contents("module.txt")
+                getData = getModuleCon.decoded_content.decode()
+                parseData = json.loads(getData)
+                ModuleName = parseData['id']
+                ModuleDir = os.mkdir(IndexDir+ModuleName)
+                ModuleDirSrc = IndexDir+ModuleName
 
-              # write module.txt in module directory
+                # write module.txt in module directory
+                ModuleFile = open(ModuleDirSrc+"/module.txt", "a")
+                ModuleFile.write(getData)
+                ModuleFile.close()
 
-              ModuleFile= open(ModuleDirSrc+"/module.txt", "a")
-              ModuleFile.write(getData)
-              ModuleFile.close()
+                # search for readme files
+                readmeFound = 0
+                for Readme in con:
+                    if(Readme.path == "README.md" or Readme.path == "README.MD" or Readme.path == "README.markdown"):
+                        print("Readme Found")
+                        readmeFound += 1
+                        ModuleReadmeCon = repository.get_contents(Readme.path)
+                        getReadmeData = ModuleReadmeCon.decoded_content.decode()
+                        ModuleReadmeFile = open(ModuleDirSrc+"/README.md", "a")
+                        ModuleReadmeFile.write(getReadmeData)
+                        ModuleReadmeFile.close()
 
-              # search for readme files
-              readmeFound=0
-              for Readme in con:
-                  if(Readme.path=="README.md" or Readme.path=="README.MD" or Readme.path=="README.markdown"):
-                      print("Readme Found")
-                      readmeFound+=1
-                      ModuleReadmeCon=repository.get_contents(Readme.path)
-                      getReadmeData=ModuleReadmeCon.decoded_content.decode()
-                      ModuleReadmeFile= open(ModuleDirSrc+"/README.md", "a")
-                      ModuleReadmeFile.write(getReadmeData)
-                      ModuleReadmeFile.close()
-                   
-              if(readmeFound==0):
-                  print("No Readme found")
-              
-              bannerFound=0
-              for banner in con:
-                  if(banner.type=="dir"):
-                      bannerCon=con.extend(repository.get_contents(banner.path))
-                  else:
-                      if(banner.path=="docs/_media/banner.png" or banner.path=="docs/_media/banner.jpg"):
-                          bannerFound+=1
-                          bannerImage="https://raw.githubusercontent.com/Terasology/"+ModuleName+"/develop/"+banner.path
-                          response = requests.get(bannerImage)
-                          imageFile = open(ModuleDirSrc+"/logo.jpg", "wb")
-                          imageFile.write(response.content)
-                          imageFile.close()
+                if(readmeFound == 0):
+                    print("No Readme found")
 
-              if(bannerFound==0):
-                  print("Resloving with default image")
-                  defaultImage="https://avatars.githubusercontent.com/u/4554375?s=200&v=4"
-                  response = requests.get(defaultImage)
-                  defaultImageFile = open(ModuleDirSrc+"/logo.jpg", "wb")
-                  defaultImageFile.write(response.content)
-                  defaultImageFile.close()
+                bannerFound = 0
+                for banner in con:
+                    if(banner.type == "dir"):
+                        bannerCon = con.extend(
+                            repository.get_contents(banner.path))
+                    else:
+                        if(banner.path == "docs/_media/banner.png" or banner.path == "docs/_media/banner.jpg"):
+                            print("Image Found")
+                            bannerFound += 1
+                            bannerImage = "https://raw.githubusercontent.com/Terasology/" + \
+                                ModuleName+"/develop/"+banner.path
+                            response = requests.get(bannerImage)
+                            imageFile = open(ModuleDirSrc+"/logo.jpg", "wb")
+                            imageFile.write(response.content)
+                            imageFile.close()
+
+                if(bannerFound == 0):
+                    print("Resloving with default image")
+                    defaultImage = "https://avatars.githubusercontent.com/u/4554375?s=200&v=4"
+                    response = requests.get(defaultImage)
+                    defaultImageFile = open(ModuleDirSrc+"/logo.jpg", "wb")
+                    defaultImageFile.write(response.content)
+                    defaultImageFile.close()
+            else:
+                print("repository is not a module")

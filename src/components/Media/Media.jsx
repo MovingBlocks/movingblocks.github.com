a84ea98b312e8e4d95bc9b8e565f389375e1114c
ryuk156 <yp15601560@gmail.com>
2021-07-06 14:40:25 +0530

    Light box implementation

--- src/components/Media/Media.jsx
+++ src/components/Media/Media.jsx
@@ -1,75 +1,134 @@
-import React ,{useEffect, useRef} from "react";
+import React, { useEffect, useState } from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
-import { Row, Col } from 'reactstrap';
-import { useStaticQuery, graphql } from "gatsby"
+import { Row, Col } from "reactstrap";
+import { useStaticQuery, graphql } from "gatsby";
+import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 
 const Gallery = () => {
-  const imageZoomed = useRef()
-  const modal = useRef()
-  const modalImg = useRef()
-
-  useEffect(() => {
-    document.addEventListener("click", (event)=>{
-      if(event.target == modal.current && imageZoomed.current == true){
-        modal.current.style.display = 'none'
-        imageZoomed.current = false
-      }
-      
-    });
-  }, []);
-
-
-  const ZoomImages = (src)=>{
-    imageZoomed.current = true
-    modal.current.style.display = 'block'
-    modalImg.current.src = src
-  }
   const data = useStaticQuery(graphql`
-  query Images{
-    images: allFile(filter: {relativeDirectory: { eq: "images"} }){
-      nodes {
-        id
-        name
-        childImageSharp {
-          gatsbyImageData
+    query Images {
+      images: allFile(filter: { relativeDirectory: { eq: "images" } }) {
+        nodes {
+          id
+          name
+          childImageSharp {
+            gatsbyImageData(width: 850, placeholder: BLURRED)
+          }
+        }
       }
     }
-  }
-  }
-`
-  )
-  console.log(data);
+  `);
+
+  let imgArray = [];
+  data.images.nodes.map((img) => {
+    imgArray.push(img.childImageSharp.gatsbyImageData);
+  });
+
+  const [imageDisplay, setImageDisplay] = useState(false);
+  const [imageToShow, setImageToShow] = useState(``);
+  const [blur, setblur] = useState(``);
+
+  const showImage = (image) => {
+    setImageToShow(image);
+    setImageDisplay(true);
+    setblur("backblur");
+  };
+
+  const hideImage = () => {
+    setImageDisplay(false);
+    setblur("");
+  };
+
+  const showNext = (e) => {
+    e.stopPropagation();
+    let currentIndex = imgArray.indexOf(imageToShow);
+    if (currentIndex >= imgArray.length - 1) {
+      setImageDisplay(false);
+      setblur("");
+    } else {
+      let nextImage = imgArray[currentIndex + 1];
+      setImageToShow(nextImage);
+    }
+  };
+
+  const showPrev = (e) => {
+    e.stopPropagation();
+    let currentIndex = imgArray.indexOf(imageToShow);
+    if (currentIndex <= 0) {
+      setImageDisplay(false);
+      setblur("");
+    } else {
+      let nextImage = imgArray[currentIndex - 1];
+      setImageToShow(nextImage);
+    }
+  };
+
+  const [currentPage,setCurrentPage]=useState(1)
+  const [imagePerPage,setImagePerPage]=useState(9)
+
+  const indexOfLastImage=currentPage*imagePerPage
+  const indexOfFirstImage= indexOfLastImage-imagePerPage
+  const currentImgArray=imgArray.slice(indexOfFirstImage,indexOfLastImage)
+
   return (
-    <section>
-      <Row className="justify-content-center">
-        
-    
-            {data.images.nodes.map(image => (
-               <Col lg="4" md="4" sm="6" xs="6" >
-              <button class="btnImage" onClick={(e) => ZoomImages(image.childImageSharp.gatsbyImageData.images.fallback.src)}>
-               
-                <GatsbyImage
-                    image={
-                      image.childImageSharp.gatsbyImageData
-                    }
-                    imgStyle={{
-                      borderRadius: "0px 0px 0px 0px",
-                      cursor: "initial",
-                    }}
-                  />
-              
+    <div>
+      <div className={blur}>
+        <h1 className="text-center">Screenshots</h1>
+        <div className="container my-4">
+          <div className="home-underline"></div>
+        </div>
+      </div>
+
+      <div>
+        <Row className="justify-content-center">
+          {currentImgArray.map((image) => (
+            <Col
+              lg="4"
+              md="6"
+              sm="6"
+              xs="6"
+              className={blur}
+              onClick={() => showImage(image)}
+            >
+              <div className="media-img">
+                {" "}
+                <GatsbyImage image={image} />
+              </div>
+            </Col>
+          ))}
+
+          {imageDisplay ? (
+            <div className=" lightbox">
+              <button
+                className="btn btn-lg btn-primary ml-3 font-weigth-bolder"
+                onClick={showPrev}
+              >
+                {" "}
+                <FiArrowLeft />
               </button>
-              </Col>
-            ))}
-         
-          <div id="imageModal" class="modal" ref={modal}>
-            <img class="modal-content" id="imageZoom" ref={modalImg}></img>
-          </div>
-          
-       </Row>
-    </section>
-  )
-}
+              <div>
+                <GatsbyImage
+                  image={imageToShow}
+                  id="lightbox-img"
+                  onClick={hideImage}
+                  className="m-4"
+                />
+              </div>
 
-export default Gallery
+              <button
+                className="btn btn-lg btn-primary ml-3 font-weigth-bolder"
+                onClick={showNext}
+              >
+                <FiArrowRight />
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
+        </Row>
+      </div>
+    </div>
+  );
+};
 
+export default Gallery;

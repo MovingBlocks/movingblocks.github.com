608855a210c2528dd3381abb685bf523fb110864
ryuk156 <yp15601560@gmail.com>
2021-07-08 01:18:47 +0530

    fix: refactor scrape.py

--- Module-generation/scrape.py
+++ Module-generation/scrape.py
@@ -3,8 +3,8 @@ import json
 import requests
 from github import Github
 
-IndexDir = "./meta-data/"
-os.mkdir(IndexDir)
+indexDir = "./meta-data/"
+os.mkdir(indexDir)
 
 accessToken = os.environ.get('GIT_TOKEN')
 g = Github(accessToken)
@@ -14,52 +14,54 @@ repo = user.get_repo("Index")
 allModules = repo.get_contents("/distros/omega/gradle.properties")
 getModules = allModules.decoded_content.decode()
 for line in getModules.split():
-    if line.startswith('extraModules='):
+    if  line.startswith('extraModules='):
         filterData=line[13:]
-        Modules=filterData.split(",")
+        modules=filterData.split(",")
 
-# Collect modules
-for module in range(len(Modules)):
-    if(module < len(Modules)):
-        repository = user.get_repo(Modules[module])
-
-        # Fetch module Information
+# Collect modules 
+for module in range(len(modules)):
+    if(module<2):
+        repository=user.get_repo(modules[module])
+        
+        #Fetch module Information
         try:
-            moduleContent = repository.get_contents("module.txt")
-            getModuleData = moduleContent.decoded_content.decode()
-            parseModuleData = json.loads(getModuleData)
-            ModuleName = parseModuleData['id']
-            ModuleDir = os.mkdir(IndexDir+ModuleName)
-            ModuleDirSrc = IndexDir+ModuleName
-            ModuleFile = open(ModuleDirSrc+"/module.txt", "a")
-            ModuleFile.write(getModuleData)
-            ModuleFile.close()
+            moduleContent=repository.get_contents("module.txt")
+            getModuleData=moduleContent.decoded_content.decode()
+            parseModuleData=json.loads(getModuleData)
+            moduleName=parseModuleData['id']
+            moduleDir=os.mkdir(indexDir+moduleName)
+            moduleDirSrc=indexDir+moduleName
+            moduleFile= open(moduleDirSrc+"/module.txt", "a")
+            moduleFile.write(getModuleData)
+            moduleFile.close()
         except:
-            print("Repository is not a Module "+ModuleName)
+            print("Repository is not a Module "+moduleName)
 
-        # Fetch Readme file
+        #Fetch Readme file
         try:
-            readmeContent = repository.get_contents("README.md")
-            getReadmeData = readmeContent.decoded_content.decode()
-            ModuleReadmeFile = open(ModuleDirSrc+"/README.md", "a")
-            ModuleReadmeFile.write(getReadmeData)
-            ModuleReadmeFile.close()
+            readmeContent=repository.get_contents("README.md")
+            getReadmeData=readmeContent.decoded_content.decode()
+            moduleReadmeFile= open(moduleDirSrc+"/README.md", "a")
+            moduleReadmeFile.write(getReadmeData)
+            moduleReadmeFile.close()
         except:
-            print("No Readme Found "+ModuleName)
+            print("No Readme Found "+moduleName)
+            moduleDefaultReadme= open(moduleDirSrc+"/README.md", "a")
+            moduleDefaultReadme.write("No information available of "+ moduleName)
+            moduleDefaultReadme.close()
 
-        # Fetch banner image
-        bannerImage = "https://raw.githubusercontent.com/Terasology/" + \
-            ModuleName+"/develop/docs/_media/banner.png"
+        #Fetch banner image
+        bannerImage="https://raw.githubusercontent.com/Terasology/"+moduleName+"/develop/docs/_media/banner.png"
         response = requests.get(bannerImage)
         if(response.ok):
-            imageFile = open(ModuleDirSrc+"/cover.png", "wb")
+            imageFile = open(moduleDirSrc+"/cover.png", "wb")
             imageFile.write(response.content)
             imageFile.close()
         else:
-            print("No banner found on "+ModuleName +
-                  ",resolving with default banner")
-            sourceImage = open("defaultBanner.png", "rb+")
-            readSourceImage = sourceImage.read()
-            defaultImageFile = open(ModuleDirSrc+"/cover.png", "wb+")
+            print("No banner found on "+ moduleName+",resolving with default banner")
+            sourceImage=open("defaultBanner.png", "rb+")
+            readSourceImage=sourceImage.read()
+            defaultImageFile = open(moduleDirSrc+"/cover.png", "wb+")
             defaultImageFile.write(readSourceImage)
             defaultImageFile.close()
+            

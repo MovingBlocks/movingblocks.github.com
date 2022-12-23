68fb90138c80e5a3cfe9fbc5b9b45fdc6eb0e5f1
Josephine Rueckert <jd.rueckert@googlemail.com>
2022-12-24 00:31:37 +0100

    chore: fix eslint findings for gsoc_tsoc.jsx

--- src/pages/gsoc_tsoc.jsx
+++ src/pages/gsoc_tsoc.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import Layout from "../layout";
 import GsocTsoc from "../components/GsocTsoc/GsocTsoc";
 
-const gsoc_tsoc = () => (
+const gsocTsoc = () => (
   <Layout>
     <GsocTsoc />
   </Layout>
 );
 
-export default gsoc_tsoc;
+export default gsocTsoc;

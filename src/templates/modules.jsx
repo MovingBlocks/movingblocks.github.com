9b75dbcf0e9d052196af548725370b3ce9c0c5fd
Josephine Rueckert <jd.rueckert@googlemail.com>
2022-12-24 00:04:26 +0100

    chore: fix eslint findings for modules.jsx

--- src/templates/modules.jsx
+++ src/templates/modules.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
+import { GatsbyImage } from "gatsby-plugin-image";
 import Layout from "../layout";
 import PostTags from "../components/PostTags/PostTags";
 import SocialLinks from "../components/SocialLinks/SocialLinks";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
-import { GatsbyImage } from "gatsby-plugin-image";
 
 export default class ModuleTemplate extends React.Component {
   render() {
@@ -29,19 +29,19 @@ export default class ModuleTemplate extends React.Component {
           <SEO postPath={slug} postNode={postNode} postSEO />
           <div>
             <GatsbyImage
-              className={"post-cover"}
+              className="post-cover"
               image={post.cover.childImageSharp.gatsbyImageData}
             />
             <h1>{post.title}</h1>
             <div className="d-flex mt-2 ml-2">
               {post.tags.map((tag) => {
-                return <PostTags tags={tag} type={"modules"} />;
+                return <PostTags tags={tag} type="modules" />;
               })}
             </div>
-            <hr></hr>
+            <hr />
             <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
             <div className="post-meta">
-              <SocialLinks postPath={"/modules" + slug} postNode={postNode} />
+              <SocialLinks postPath={`/modules${slug}`} postNode={postNode} />
             </div>
           </div>
         </div>

7214056c39787399df0ccab04917e9498ef4f190
Tobias Nett <skaldarnar@gmail.com>
2022-12-28 22:45:45 +0100

    build(perf): generate smaller search indices

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
-const kebabCase = require("lodash.kebabcase");
 const fs = require("fs");
+const kebabCase = require("lodash.kebabcase");
 const moment = require("moment");
 const path = require("path");
 const siteConfig = require("./data/SiteConfig");
@@ -50,94 +50,207 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions;
 
-  const blogPostTemplate = path.resolve("src/templates/post.jsx");
-  const blogQueryResult = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          filter: { frontmatter: { posttype: { eq: "blog" } } }
-        ) {
-          edges {
-            node {
-              fields {
-                slug
+  async function createBlogPages() {
+    const blogPostTemplate = path.resolve("src/templates/post.jsx");
+    const blogQueryResult = await graphql(
+      `
+        {
+          allMarkdownRemark(
+            filter: { frontmatter: { posttype: { eq: "blog" } } }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
               }
             }
           }
         }
-      }
-    `
-  );
-  const posts = blogQueryResult.data.allMarkdownRemark.edges;
-  posts.forEach((edge) => {
-    createPage({
-      path: `/blog${edge.node.fields.slug}`,
-      component: blogPostTemplate,
-      context: {
-        slug: edge.node.fields.slug,
-      },
+      `
+    );
+    const posts = blogQueryResult.data.allMarkdownRemark.edges;
+    posts.forEach((edge) => {
+      createPage({
+        path: `/blog${edge.node.fields.slug}`,
+        component: blogPostTemplate,
+        context: {
+          slug: edge.node.fields.slug,
+        },
+      });
     });
-  });
 
-  const blogListTemplate = path.resolve("./src/templates/blog.jsx");
-  const blogsPerPage = 27;
-  const numBlogPages = Math.ceil(posts.length / blogsPerPage);
-  Array.from({ length: numBlogPages }).forEach((_, i) => {
-    createPage({
-      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
-      component: blogListTemplate,
-      context: {
-        limit: blogsPerPage,
-        skip: i * blogsPerPage,
-        postsNumPages: numBlogPages,
-        blogCurrentPage: i + 1,
-      },
+    const blogListTemplate = path.resolve("./src/templates/blog.jsx");
+    const blogsPerPage = 27;
+    const numBlogPages = Math.ceil(posts.length / blogsPerPage);
+    Array.from({ length: numBlogPages }).forEach((_, i) => {
+      createPage({
+        path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+        component: blogListTemplate,
+        context: {
+          limit: blogsPerPage,
+          skip: i * blogsPerPage,
+          postsNumPages: numBlogPages,
+          blogCurrentPage: i + 1,
+        },
+      });
     });
-  });
+  }
 
-  const modulePageTemplate = path.resolve("src/templates/modules.jsx");
-  const moduleQueryResult = await graphql(
-    `
-      {
-        allMarkdownRemark(
-          filter: { frontmatter: { posttype: { eq: "module" } } }
-        ) {
-          edges {
-            node {
-              fields {
-                slug
+  async function createModulePages() {
+    const modulePageTemplate = path.resolve("src/templates/modules.jsx");
+    const moduleQueryResult = await graphql(
+      `
+        {
+          allMarkdownRemark(
+            filter: { frontmatter: { posttype: { eq: "module" } } }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
               }
             }
           }
         }
+      `
+    );
+
+    const modules = moduleQueryResult.data.allMarkdownRemark.edges;
+    modules.forEach((edge) => {
+      createPage({
+        path: `/modules${edge.node.fields.slug}`,
+        component: modulePageTemplate,
+        context: {
+          slug: edge.node.fields.slug,
+        },
+      });
+    });
+
+    const moduleListTemplate = path.resolve("./src/templates/modulelist.jsx");
+    const modulesPerPage = 27;
+    const numModulePages = Math.ceil(modules.length / modulesPerPage);
+    Array.from({ length: numModulePages }).forEach((_, i) => {
+      createPage({
+        path: i === 0 ? `/modules` : `/modules/${i + 1}`,
+        component: moduleListTemplate,
+        context: {
+          limit: modulesPerPage,
+          skip: i * modulesPerPage,
+          moduleNumPages: numModulePages,
+          moduleCurrentPage: i + 1,
+        },
+      });
+    });
+  }
+
+  await createBlogPages();
+  await createModulePages();
+
+  // TODO: replace below with proper search index generation, see
+  //          https://www.gatsbyjs.com/docs/how-to/adding-common-features/adding-search/
+
+  async function buildBlogSearchIndex() {
+    const blogSearchIndexQueryResult = await graphql(
+      `
+        {
+          allMarkdownRemark(
+            filter: { frontmatter: { posttype: { eq: "blog" } } }
+          ) {
+            edges {
+              node {
+                fields {
+                  slug
+                }
+                frontmatter {
+                  author
+                  date
+                  tags
+                  title
+                }
+                internal {
+                  content
+                }
+              }
+            }
+          }
+        }
+      `
+    );
+    const index = blogSearchIndexQueryResult.data.allMarkdownRemark.edges.map(
+      (edge) => {
+        const { fields, frontmatter, internal } = edge.node;
+        const { slug } = fields;
+        const { title, tags, author, date } = frontmatter;
+        const { content } = internal;
+
+        return {
+          author,
+          content,
+          date,
+          slug,
+          tags,
+          title,
+        };
       }
-    `
+    );
+
+    return index;
+  }
+
+  async function buildModulesSearchIndex() {
+    const result = await graphql(
+      `
+        {
+          allMarkdownRemark(
+            filter: { frontmatter: { posttype: { eq: "module" } } }
+          ) {
+            edges {
+              node {
+                excerpt
+                fields {
+                  slug
+                }
+                frontmatter {
+                  tags
+                  title
+                }
+              }
+            }
+          }
+        }
+      `
+    );
+    const index = result.data.allMarkdownRemark.edges.map((edge) => {
+      const { excerpt, fields, frontmatter } = edge.node;
+      const { slug } = fields;
+      const { tags, title } = frontmatter;
+
+      return {
+        excerpt,
+        slug,
+        tags,
+        title,
+      };
+    });
+
+    return index;
+  }
+
+  const blogIndex = await buildBlogSearchIndex();
+  const moduleIndex = await buildModulesSearchIndex();
+
+  if (!fs.existsSync("src/generated")) {
+    fs.mkdirSync("src/generated");
+  }
+
+  fs.writeFileSync(
+    "./src/generated/blog-result.json",
+    JSON.stringify(blogIndex, null, 2)
   );
-
-  const modules = moduleQueryResult.data.allMarkdownRemark.edges;
-  modules.forEach((edge) => {
-    createPage({
-      path: `/modules${edge.node.fields.slug}`,
-      component: modulePageTemplate,
-      context: {
-        slug: edge.node.fields.slug,
-      },
-    });
-  });
-
-  const moduleListTemplate = path.resolve("./src/templates/modulelist.jsx");
-  const modulesPerPage = 27;
-  const numModulePages = Math.ceil(modules.length / modulesPerPage);
-  Array.from({ length: numModulePages }).forEach((_, i) => {
-    createPage({
-      path: i === 0 ? `/modules` : `/modules/${i + 1}`,
-      component: moduleListTemplate,
-      context: {
-        limit: modulesPerPage,
-        skip: i * modulesPerPage,
-        moduleNumPages: numModulePages,
-        moduleCurrentPage: i + 1,
-      },
-    });
-  });
+  fs.writeFileSync(
+    "./src/generated/module-result.json",
+    JSON.stringify(moduleIndex, null, 2)
+  );
 };

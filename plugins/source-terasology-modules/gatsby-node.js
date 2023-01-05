4c263be4f1e6a347c389eed9a8a8641c7f58d7c1
Tobias Nett <skaldarnar@gmail.com>
2023-01-05 23:23:51 +0100

    feat(source-terasology-modules): add source plugin to fetch Terasology module info (#179)
    - feat(source-terasology-module): custom source plugin to fetch Terasology module information
      - basic information: name, url, description,  homepageUrl
      - include URL to custom `cover` image if defined
      - parse `module.txt` and enrich it with `tags` (also skip repos without 'module.txt' on branch 'develop')
      - include content of `README.md`
    - chore: build with `NODE_OPTIONS='--no-experimental-fetch'` to avoid warning about experimental Fetch API in Node 18.x (see https://github.com/octokit/request.js/issues/526 and https://github.com/node-fetch/node-fetch/issues/1566)
    - doc(source-terasology-module): document basic usage of the source plugin

---
+++ plugins/source-terasology-modules/gatsby-node.js
@@ -0,0 +1,169 @@
+const { graphql } = require("@octokit/graphql");
+const { DateTime } = require("luxon");
+
+const PLUGIN_NAME = "source-terasology-modules";
+
+const query = `
+query Modules($cursor:String) {
+  organization(login: "Terasology") {
+    repositories(first: 20, after: $cursor) {
+      nodes {
+        id
+        name
+        url
+        description
+        homepageUrl
+        openGraphImageUrl
+        usesCustomOpenGraphImage
+
+        moduleTxt: object(expression: "develop:module.txt") {
+          ...on Blob {
+            text
+          }
+        }
+
+        readme: object(expression: "develop:README.md") {
+          ...on Blob {
+            text
+          }
+        }
+      }
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+    }
+  }
+}
+`;
+
+exports.onPreInit = () => console.log("Loaded source-terasology-modules");
+
+exports.sourceNodes = async (
+  {
+    actions: { createNode },
+    createContentDigest,
+    createNodeId,
+    reporter,
+    cache,
+  },
+  { accessToken }
+) => {
+  const gql = graphql.defaults({
+    headers: {
+      authorization: `token ${accessToken}`,
+    },
+  });
+
+  const lastFetchedKey = "terasology-modules-last-fetched";
+  const dataKey = "terasology-modules-data";
+
+  const now = DateTime.utc();
+  const lastFetched = DateTime.fromISO(await cache.get(lastFetchedKey), {
+    zone: "utc",
+  });
+
+  let repositories = [];
+
+  if (lastFetched.plus({ days: 1 }) > now) {
+    reporter.info(
+      `[${PLUGIN_NAME}] Loading Terasology module info from cache ...`
+    );
+    repositories = JSON.parse(await cache.get(dataKey));
+  } else {
+    reporter.info(
+      `[${PLUGIN_NAME}] Fetching Terasology module info from GitHub ...`
+    );
+    let hasNextPage = true;
+    let cursor;
+
+    /* Loop iterations depend on the outcome of the previous request, thus 'await' is required here. */
+    /* eslint-disable no-await-in-loop */
+    while (hasNextPage) {
+      const { organization } = await gql(query, { cursor });
+
+      organization.repositories.nodes.forEach((repo) =>
+        repositories.push(repo)
+      );
+
+      hasNextPage = organization.repositories.pageInfo.hasNextPage;
+      cursor = organization.repositories.pageInfo.endCursor;
+    }
+
+    await cache.set(dataKey, JSON.stringify(repositories));
+    await cache.set(lastFetchedKey, now.toISO());
+  }
+
+  reporter.success(`[${PLUGIN_NAME}] Loaded ${repositories.length} modules.`);
+
+  let created = 0;
+  repositories
+    .forEach((repo) => {
+      if (!repo.moduleTxt) {
+        // skip non-module repositories
+        return;
+      }
+
+      let moduleTxt;
+      try {
+        moduleTxt = JSON.parse(repo.moduleTxt?.text);
+      } catch (err) {
+        console.warn(
+          `[${PLUGIN_NAME}] Could not parse 'module.txt' of ${repo.url}.`
+        );
+        return;
+      }
+
+      const tags = [
+        moduleTxt.isTutorial ? "Tutorial" : undefined,
+        moduleTxt.isAsset ? "Asset" : undefined,
+        moduleTxt.isLibrary ? "Library" : undefined,
+        moduleTxt.isSpecial ? "Special" : undefined,
+        moduleTxt.isRendering ? "Rendering" : undefined,
+        moduleTxt.isAugmentation ? "Augmentation" : undefined,
+        moduleTxt.isServerSideOnly ? "Server" : undefined,
+        moduleTxt.isGameplay ? "Gameplay" : undefined,
+      ].filter((x) => x);
+
+      const { id, version, displayName, description, dependencies } = moduleTxt;
+
+      const cover = repo.usesCustomOpenGraphImage
+        ? repo.openGraphImageUrl
+        : undefined;
+
+      const moduleInfo = {
+        id,
+        version,
+        displayName,
+        description,
+        dependencies,
+        tags,
+      };
+
+      moduleInfo.dependencies = moduleInfo.dependencies.map((dep) => ({
+        ...dep,
+        optional: /true/i.test(dep.optional),
+      }));
+
+      const node = {
+        ...repo,
+        usesCustomOpenGraphImage: undefined,
+        openGraphImageUrl: undefined,
+        cover,
+        moduleTxt: moduleInfo,
+        id: createNodeId(`TerasologyModule-${repo.id}`),
+        parent: "__SOURCE__",
+        children: [],
+        internal: {
+          type: "TerasologyModule",
+        },
+      };
+      node.internal.contentDigest = createContentDigest(node);
+
+      createNode(node);
+      created += 1;
+    })
+    .filter((x) => x);
+
+  reporter.success(`[${PLUGIN_NAME}] Created ${created} nodes.`);
+};

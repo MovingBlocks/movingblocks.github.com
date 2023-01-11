fe2b4279b825764b2a4d427f8de453225ea3fc88
Tobias Nett <skaldarnar@gmail.com>
2023-01-11 23:45:45 +0100

    [WIP] load more, but its breaking search...

--- src/pages/modules/search.jsx
+++ src/pages/modules/search.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { graphql, navigate } from "gatsby";
 import { Button, Col, Row, Form, FormGroup, Label, Input } from "reactstrap";
 import SEO from "../../components/SEO/SEO";
@@ -6,10 +6,9 @@ import Layout from "../../layout";
 import ModuleListing from "../../components/modules/ModuleListing";
 import withLocation from "../../components/common/withLocation";
 import useJsSearch from "../../components/modules/useJsSearch";
-import ModuleSearchForm from "components/ModuleSearchForm/ModuleSearchForm";
 import Select from "react-select";
 
-// TODO: we really need to pass 'navigate' on or would the default from Gatsby suffice?
+// TODO: factor this out into it's own component in components/modules.
 function SearchForm({ searchParams, allTags }) {
   const options = allTags.sort().map((t) => ({ value: t, label: t }));
 
@@ -31,7 +30,10 @@ function SearchForm({ searchParams, allTags }) {
                 e.forEach((t) => searchParams.append("tag", t.value));
                 navigate(`?${searchParams.toString()}`);
               }}
-              defaultValue={searchParams.getAll("tag").filter(x=>x).map(t => ({value: t, label: t}))}
+              defaultValue={searchParams
+                .getAll("tag")
+                .filter((x) => x)
+                .map((t) => ({ value: t, label: t }))}
             />
           </FormGroup>
         </Col>
@@ -82,9 +84,31 @@ function Search({ data, pageContext, searchParams }) {
   // filter based on searched term
   const searchResults = term ? search(term) : allModules;
   // filter based on tags
-  const modules = searchResults.filter((m) =>
+
+  const filteredModules = searchResults.filter((m) =>
     tags.every((t) => m.moduleTxt.tags.includes(t))
   );
+
+  // "Load More" functionality based on https://www.erichowey.dev/writing/load-more-button-and-infinite-scroll-in-gatsby/
+  const [modules, setModules] = useState(filteredModules.slice(0, 15));
+  const [loadMore, setLoadMore] = useState(false);
+  const [hasMore, setHasMore] = useState(allModules.length > 15);
+
+  function handleLoadMore() {
+    setLoadMore(true);
+  }
+
+  useEffect(() => {
+    if (loadMore && hasMore) {
+      const nextModules = allModules.slice(0, modules.length + 15);
+      setModules(nextModules);
+    }
+    setLoadMore(false);
+  }, [loadMore, hasMore]);
+
+  useEffect(() => {
+    setHasMore(modules.length < filteredModules.length);
+  }, [modules]);
 
   return (
     <Layout title="Modules">
@@ -97,6 +121,11 @@ function Search({ data, pageContext, searchParams }) {
         <Row className="justify-content-center">
           <ModuleListing defaultCover={data.defaultCover} modules={modules} />
         </Row>
+        {hasMore ? (
+          <Row className="justify-content-center">
+            <Button onClick={handleLoadMore}>Load More</Button>
+          </Row>
+        ) : null}
       </div>
     </Layout>
   );

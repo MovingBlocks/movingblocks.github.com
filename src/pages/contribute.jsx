da34ac492a74f3697d00d0a19503d72fbb229487
Josephine Rueckert <jd.rueckert@googlemail.com>
2023-01-12 00:07:09 +0100

    feat: add good first engine issues

--- src/pages/contribute.jsx
+++ src/pages/contribute.jsx
@@ -49,6 +49,17 @@ function GettingStarted({ data }) {
     })
     .sort((a, b) => { return new Date(a.date).getTime() <= new Date(b.date).getTime() ? 1 : 0; });
 
+    const engine = data.engineIssues.nodes[0]
+    const engineIssues = engine.issues.nodes
+    .map((issue) => {
+      const { title, author, labels, updatedAt, url } = issue;
+      const { login } = author;
+      const { nodes } = labels;
+      const tags = nodes.flatMap((node) => node.name)
+      return { title, author: login, tags, date: updatedAt, url };
+    })
+    .sort((a, b) => { return new Date(a.date).getTime() <= new Date(b.date).getTime() ? 1 : 0; });
+
   return (
     <Layout title="Getting Contributors Started">
       <Row className="justify-content-center align-items-start">
@@ -331,15 +342,14 @@ function GettingStarted({ data }) {
                   Rendering Package
                 </a>
               </li>
-              <li>
-                <a
-                  className="text-success"
-                  href="https://github.com/MovingBlocks/Terasology/issues?q=is%3Aopen+is%3Aissue+label%3A%22Good+First+Issue%22"
-                >
-                  Good First Engine Issues
-                </a>
-              </li>
             </ul>
+            <p>
+              Jump below to our{" "}
+              <Link to="#engineIssues" className="text-success">
+                <b>Good First Issues in Engine</b>
+              </Link>
+              .
+            </p>
           </Col>
         </Row>
       </Section>
@@ -383,8 +393,8 @@ function GettingStarted({ data }) {
           <Row className="justify-content-center align-items-start">
             <Col md="8" className="text-justify justify-content-center">
               <p>
-                Find our module-land issues below. If you'd like to work on one of them, 
-                start a draft PR for it. You can also view them on{" "}
+                Find some of our module-land issues below. If you'd like to work on one of them, 
+                start a draft PR for it. You can also view the full list on{" "}
                 <a
                   className="text-success font-weight-bold"
                   href="https://github.com/search?q=org%3ATerasology+label%3A%22Good+First+Issue%22+state%3Aopen&type=Issues&ref=advsearch&l=&l="
@@ -396,7 +406,7 @@ function GettingStarted({ data }) {
             </Col>
           </Row>
           <Col lg="12" className="card-spacing">
-            {moduleIssues.map(
+            {moduleIssues.slice(0, 10).map(
               ({ module, moduleUrl, title, tags, author, url, date }) => (
                 <Row className="justify-content-start align-items-start">
                   <Card className="row_shadow h-100 md-12 my-3" style={{ width: '100%' }}>
@@ -405,6 +415,73 @@ function GettingStarted({ data }) {
                         {module}
                       </CardHeader>
                     </a>
+                    <a href={url} className="btn-light" target="_blank" rel="noreferrer">
+                      <Row className="justify-content-start align-items-center">
+                        <Col md="1" className="ml-5 pt-0 pb-2">
+                          <img src={gitHubLogo} class="img-fluid rounded-start" alt="Visit on GitHub" />
+                        </Col>
+                        <Col md="10" className="pt-0 pb-2">
+                          <CardBody>
+                            {tags ? (
+                              <CardSubtitle tag="h7">
+                                <Tags tags={tags} />
+                              </CardSubtitle>
+                            ) : (
+                              ""
+                            )}
+                            <CardTitle tag="h5" className="mt-3">
+                              {title}
+                            </CardTitle>
+                            {author ? (
+                              <CardSubtitle className="text-muted">
+                                <b>Author:</b> {author}
+                              </CardSubtitle>
+                            ) : (
+                              ""
+                            )}
+                            {date ? (
+                              <CardSubtitle className="text-muted">
+                                <b>Last updated on: </b>{moment(date).format("MMMM DD, YYYY")}
+                              </CardSubtitle>
+                            ) : (
+                              ""
+                            )}
+                          </CardBody>
+                        </Col>
+                      </Row>
+                    </a>
+                  </Card>
+                </Row>
+              ))
+            }
+          </Col>
+        </Section>
+        <Section
+          tag="h4"
+          id="engineIssues"
+          title="Good First Engine Issues"
+        >
+          <Row className="justify-content-center align-items-start">
+            <Col md="8" className="text-justify justify-content-center">
+              <p>
+                Find some of our engine issues below. If you'd like to work on one of them, 
+                start a draft PR for it. You can also view the full list on{" "}
+                <a
+                  className="text-success font-weight-bold"
+                  href="https://github.com/MovingBlocks/Terasology/issues?q=is%3Aopen+is%3Aissue+label%3A%22Good+First+Issue%22"
+                >
+                  GitHub
+                </a>
+                .
+              </p>
+            </Col>
+          </Row>
+          <Col lg="12" className="card-spacing">
+            {engineIssues.map(
+              ({ title, tags, author, url, date }) => (
+                <Row className="justify-content-start align-items-start">
+                  <Card className="row_shadow h-100 md-12 my-3" style={{ width: '100%' }}>
+                    <CardHeader className="bg-success"></CardHeader>
                     <a href={url} className="btn-light" target="_blank" rel="noreferrer">
                       <Row className="justify-content-start align-items-center">
                         <Col md="1" className="ml-5 pt-0 pb-2">
@@ -474,6 +551,26 @@ export const pageQuery = graphql`
       nodes {
         name
         url
+        issues {
+          nodes {
+            id
+            title
+            author {
+              login
+            }
+            labels {
+              nodes {
+                name
+              }
+            }
+            updatedAt
+            url
+          }
+        }
+      }
+    }
+    engineIssues: allTerasologyEngine {
+      nodes {
         issues {
           nodes {
             id

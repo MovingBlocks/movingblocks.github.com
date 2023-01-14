9a6a4a4db63797d5a63df17bd43070e64007f4d6
Tobias Nett <skaldarnar@gmail.com>
2023-01-14 14:35:57 +0100

    feat: combine "search" and "load more" functionality

--- src/pages/modules/search.jsx
+++ src/pages/modules/search.jsx
@@ -9,7 +9,7 @@ import useJsSearch from "../../components/modules/useJsSearch";
 import Select from "react-select";
 
 // TODO: factor this out into it's own component in components/modules.
-function SearchForm({ searchParams, allTags }) {
+function SearchForm({ searchParams, allTags, setSearchParams }) {
   const options = allTags.sort().map((t) => ({ value: t, label: t }));
 
   return (
@@ -28,7 +28,7 @@ function SearchForm({ searchParams, allTags }) {
               onChange={(e) => {
                 searchParams.delete("tag");
                 e.forEach((t) => searchParams.append("tag", t.value));
-                navigate(`?${searchParams.toString()}`);
+                setSearchParams(searchParams);
               }}
               defaultValue={searchParams
                 .getAll("tag")
@@ -51,7 +51,7 @@ function SearchForm({ searchParams, allTags }) {
                 } else {
                   searchParams.delete("term");
                 }
-                navigate(`?${searchParams.toString()}`);
+                setSearchParams(searchParams);
               }}
               placeholder="Search..."
               value={searchParams.get("term") || ""}
@@ -67,9 +67,10 @@ export function Head({ data: { site } }) {
   return <SEO title={`Modules | ${site.metadata.title}`} />;
 }
 
-function Search({ data, pageContext, searchParams }) {
-  const allModules = data.modules.nodes;
+function Search({ data, pageContext, searchParams: initialSearchParams }) {
+  const MODULES_PER_PAGE = 9;
 
+  const allModules = data.modules.nodes;
   const allTags = allModules.reduce((acc, m) => {
     m.moduleTxt.tags.forEach((t) => acc.add(t));
     return acc;
@@ -78,44 +79,71 @@ function Search({ data, pageContext, searchParams }) {
   // build search index for all modules
   const { search } = useJsSearch(allModules);
 
-  const tags = searchParams.getAll("tag").filter((x) => x);
-  const term = searchParams.get("term") || "";
+  const [searchParams, setSearchParams] = useState(initialSearchParams);
+  // TODO: check whether we need this dirty flag (infinite rendering loops?)
+  const [searchDirty, setSearchDirty] = useState(true);
+  const [filteredModules, setFilteredModules] = useState(allModules);
 
-  // filter based on searched term
-  const searchResults = term ? search(term) : allModules;
-  // filter based on tags
-
-  const filteredModules = searchResults.filter((m) =>
-    tags.every((t) => m.moduleTxt.tags.includes(t))
-  );
-
-  // "Load More" functionality based on https://www.erichowey.dev/writing/load-more-button-and-infinite-scroll-in-gatsby/
-  const [modules, setModules] = useState(filteredModules.slice(0, 15));
-  const [loadMore, setLoadMore] = useState(false);
-  const [hasMore, setHasMore] = useState(allModules.length > 15);
-
-  function handleLoadMore() {
-    setLoadMore(true);
+  function updateSearchParams(params) {
+    setSearchParams(params);
+    setSearchDirty(true);
   }
 
   useEffect(() => {
+    if (searchDirty) {
+      const tags = searchParams.getAll("tag").filter((x) => x);
+      const term = searchParams.get("term") || "";
+      // filter based on searched term
+      const searchResults = term ? search(term) : allModules;
+      // filter based on tags
+      const result = searchResults.filter((m) =>
+        tags.every((t) => m.moduleTxt.tags.includes(t))
+      );
+      setFilteredModules(result);
+      setSearchDirty(false);
+      navigate(`?${searchParams.toString()}`);
+    }
+  }, [searchDirty, searchParams]);
+
+  // "Load More" functionality based on https://www.erichowey.dev/writing/load-more-button-and-infinite-scroll-in-gatsby/
+  const [modules, setModules] = useState(
+    filteredModules.slice(0, MODULES_PER_PAGE)
+  );
+  const [loadMore, setLoadMore] = useState(false);
+  const [hasMore, setHasMore] = useState(allModules.length > MODULES_PER_PAGE);
+
+  // reset number of shown modules when the result changes
+  useEffect(() => {
+    setModules(filteredModules.slice(0, MODULES_PER_PAGE));
+  }, [filteredModules]);
+
+  // show more results if there are more available and the user requested to load more
+  useEffect(() => {
     if (loadMore && hasMore) {
-      const nextModules = allModules.slice(0, modules.length + 15);
+      const nextModules = filteredModules.slice(
+        0,
+        modules.length + MODULES_PER_PAGE
+      );
       setModules(nextModules);
     }
     setLoadMore(false);
   }, [loadMore, hasMore]);
 
+  // compute whether there are more results to show whenever the list of results changes
   useEffect(() => {
     setHasMore(modules.length < filteredModules.length);
-  }, [modules]);
+  }, [modules, filteredModules]);
 
   return (
     <Layout title="Modules">
       <div className="module-container">
         <Row>
           <Col md="12">
-            <SearchForm searchParams={searchParams} allTags={[...allTags]} />
+            <SearchForm
+              searchParams={searchParams}
+              allTags={[...allTags]}
+              setSearchParams={updateSearchParams}
+            />
           </Col>
         </Row>
         <Row className="justify-content-center">
@@ -123,7 +151,7 @@ function Search({ data, pageContext, searchParams }) {
         </Row>
         {hasMore ? (
           <Row className="justify-content-center">
-            <Button onClick={handleLoadMore}>Load More</Button>
+            <Button onClick={() => setLoadMore(true)}>Load More</Button>
           </Row>
         ) : null}
       </div>

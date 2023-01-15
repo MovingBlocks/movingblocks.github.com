8480a78037674552f61525486ad5438e53abbd17
Tobias Nett <skaldarnar@gmail.com>
2023-01-15 14:26:22 +0100

    fix eslint warning for 'withLocation'

--- src/components/common/withLocation.jsx
+++ src/components/common/withLocation.jsx
@@ -3,19 +3,24 @@ import { Location } from "@reach/router";
 
 // Based on https://medium.com/@chrisfitkin/how-to-get-query-string-parameter-values-in-gatsby-f714161104f
 
-/* eslint-disable react/jsx-props-no-spreading - explicitly use property spreading for higher-order component (a component wrapping another component) */
-const withLocation = (ComponentToWrap) => (props) =>
-  (
-    <Location>
-      {({ location, navigate }) => (
-        <ComponentToWrap
-          {...props}
-          location={location}
-          navigate={navigate}
-          searchParams={new URLSearchParams(location.search ?? "")}
-        />
-      )}
-    </Location>
-  );
+/* Explicitly use property spreading for higher-order component (a component wrapping another component). */
+/* eslint-disable react/jsx-props-no-spreading */
+function withLocation(ComponentToWrap) {
+  /* eslint-disable func-names */
+  return function (props) {
+    return (
+      <Location>
+        {({ location, navigate }) => (
+          <ComponentToWrap
+            {...props}
+            location={location}
+            navigate={navigate}
+            searchParams={new URLSearchParams(location.search ?? "")}
+          />
+        )}
+      </Location>
+    );
+  };
+}
 
 export default withLocation;
